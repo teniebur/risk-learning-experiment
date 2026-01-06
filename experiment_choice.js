7227ffb0a6e97faa5ac3bb227a09aa95c31d4640
erik emeric <erik.emeric@gmail.com>
2026-01-06 14:48:14 -0500

    Update experiment_choice.js

--- experiment_choice.js
+++ experiment_choice.js
@@ -597,7 +597,17 @@ async function startExperiment() {
     document.getElementById('experiment-container').style.display = 'block';
     document.body.classList.add('experiment-running');
     
-    // Start
+    // Enter fullscreen automatically
+    const elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    
+    // Start first trial (or runTrial() depending on your code)
     runTrial();
 }
 
