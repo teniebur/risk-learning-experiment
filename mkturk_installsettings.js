c4fe2ab653f8295e439c77fc9892bdab3345245a
teniebur <82474383+teniebur@users.noreply.github.com>
2026-01-08 13:26:30 -0500

    Update mkturk_installsettings.js

--- mkturk_installsettings.js
+++ mkturk_installsettings.js
@@ -5,8 +5,8 @@
 // PATH & APP KEY from Dropbox Developer Console for
 // MKTURK.HTML & LIVEPLOT.HTML
 // ------ Dropbox WebApp settings ------ 
-var DBX_CLIENT_ID = "kjfmygfpzkpi5rk"
-var DBX_REDIRECT_URI_ROOT = "https://eeemeric.github.io/risk-learning-experiment/"
+var DBX_CLIENT_ID = "vqtyjae8wgvzzuv"
+var DBX_REDIRECT_URI_ROOT = "https://teniebur.github.io/risk-learning-experiment/"
 
 // ------ Subject settings ------ 
 var subjectlist = ["RiskLearningSubject"];
@@ -19,6 +19,7 @@ var SOUND_FILEPREFIX = "/mkturkfolders/sounds/au"
 // ------ Misc. -----------------------
 var ndatafiles2read=5; // todo: change to trials. and use as upper bound (stop reading once you hit the first discrepancy). maybe this is effectively synonymous with mintrials
 var num_preload_images=0; // how long can you/the NHP bother waiting at each imageload? 400 images ~ 30 seconds. Recommended to keep = 0 with good internet connection and automator on
+
 
 
 
